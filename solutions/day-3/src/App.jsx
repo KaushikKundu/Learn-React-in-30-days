805536db8fa116e8d22bfe0d71f1516139e88190
Kaushik Kundu <kaushikkundu774@gmail.com>
2023-02-23 22:54:08 +0530

    Day 3 completed

---
+++ solutions/day-3/src/App.jsx
@@ -0,0 +1,24 @@
+
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <div>
+        <img src={reactLogo} alt="" />
+      </div>
+     <h2>Kaushik Kundu</h2>
+     <p>Aspiring Full Stack Developer</p>
+     <h5>Skills</h5>
+     <button>HTML</button>
+     <button>CSS</button>
+     <button>Javascript</button>
+     <button>React JS</button>
+     
+    </div>
+  )
+}
+
+export default App
